925b1fee4a4b38df0fdd5be6b6a1cbc780041e8d
Irene Njeru <irenenjeru8@gmail.com>
2022-04-11 11:42:34 +0300

    complete task

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name){
+    return cats.pop()
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift()
+    return cats;
+}
+function appendCat(name){
+    let cat = cats.slice();
+    cat.push(name);
+    return cat;
+}
+function prependCat(name){
+    let cat = cats.slice();
+    cat.unshift(name);
+    return cat;
+}
+function removeLastCat(name){
+    let cat = [...cats];
+    cat.splice(2,1);
+    return cat;
+}
+function removeFirstCat(name){
+    let cat = [...cats];
+    cat.splice(0,1);
+    return cat;
+}
